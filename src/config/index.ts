5d43a6ef5e20e74a990b7d9c898fc3e30fe93d0a
Iam-Zarif <zarifraja19@gmail.com>
2026-05-23 19:29:42 +0600

    for frontend check set frontend port =3000, backend api=4000/5000, till api test no bug found

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ dotenv.config({
 
 const config = {
   connection_string: process.env.CONNECTION_STRING || process.env.DATABASE_URL,
-  port: process.env.PORT || "3000",
+  port: process.env.PORT || "5000",
   jwt_secret: process.env.JWT_SECRET,
   refresh_secret: process.env.REFRESH_SECRET,
 };
